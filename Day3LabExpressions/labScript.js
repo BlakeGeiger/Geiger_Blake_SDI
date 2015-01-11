0ac9c8f620e15c882fbdc4d2b4b16d6182ccd8f5
Blake <bgeiger@fullsail.edu>
2015-01-10 22:52:41 -0500

    Average shopping bill
    Average shopping bill complete

--- Day3LabExpressions/labScript.js
+++ Day3LabExpressions/labScript.js
@@ -39,6 +39,24 @@ console.log(sparky + " " + "leftover slices.");
 var answer2 = "Sparky gets 15 slices of pizza at the party.";
 console.log(answer2);
 
-// This question was unfortunate and frustrating 
+// This question was unfortunate and frustrating;
 
+// 5 grocery bills
+var bill1 = 122.55;
+var bill2 = 139.78;
+var bill3 = 128.78;
+var bill4 = 136.39;
+var bill5 = 121.91;
 
+// Average is all five added then divided by 5;
+// Don't forget PEMDAS;
+
+var totalBill = (bill1 + bill2 + bill3 + bill4 + bill5);
+console.log(totalBill);
+
+var average = totalBill / 5;
+average = average.toFixed(2);
+console.log(average);
+
+var answer3 = "You have spent a total of $649.41 on groceries over 5 weeks. That is an average of $129.88 per week";
+console.log(answer3);
