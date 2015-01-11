bee5045a2b239618114c8b089c9cb38b6e91e01a
Blake <bgeiger@fullsail.edu>
2015-01-10 22:00:56 -0500

    Slices of Pie I
    Slices of pie part one is completed.

---
+++ Day3LabExpressions/labScript.js
@@ -0,0 +1,19 @@
+/*
+Blake Geiger
+1/10/2015
+Day 3 Lab Expressions
+ */
+
+// Givens are slices, people and pizza;
+
+var slices = 8;
+var people = 16;
+var pizzas = 3;
+
+// 16 / 2 equals slices per person;
+var pizzaPeople = people / slices;
+console.log(pizzaPeople + " " + "slices per person.");
+
+// Answer
+var answer1 = "Each person ate 2 slices of pizza at the party.";
+console.log(answer1);
