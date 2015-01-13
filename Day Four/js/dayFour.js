7b0c629c2bebb5c1cd10daf6d7190f8c91b15654
Blake <bgeiger@fullsail.edu>
2015-01-13 18:16:29 -0500

    Basic Array
    avengers names

--- Day Four/js/dayFour.js
+++ Day Four/js/dayFour.js
@@ -5,5 +5,42 @@ SDI Campus
 Day 4 In Class Assignments
  */
 
-//alert("testing to see if this works");
+// alert("testing to see if this works");
+
+// Create a basic array
+var avengersNames = ["Thor", "Captain America", "Iron Man"];
+
+// Print it out to the console
+console.log(avengersNames);
+
+// Print out a specific item
+console.log(avengersNames[1]);
+
+// Switch a team member - Iron Man for Spider-Man
+avengersNames [2] = "Spider-Man";
+
+console.log(avengersNames);
+
+// Add a team member
+avengersNames[3] = "Black Widow";
+
+// Print out roster
+console.log(avengersNames);
+
+// Use a variable as an index number
+var num = 2;
+console.log(avengersNames[num]);
+
+// Length property
+// Dot syntax - fancy for use a period
+console.log(avengersNames.length);
+
+// Last index number is the length minus one
+
+// Next open space is the length
+
+// Recruit a new team member
+avengersNames[avengersNames.length] = "Powerman"
+console.log(avengersNames);
+
 
