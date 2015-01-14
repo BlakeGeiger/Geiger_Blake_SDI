8abfbfd02f8e1729742421b52acd119ca685b0fa
Blake <bgeiger@fullsail.edu>
2015-01-13 19:56:48 -0500

    User Prompting
    prompting

---
+++ Day Four/js/dayFour_prompting.js
@@ -0,0 +1,25 @@
+/*
+Blake Geiger
+SDI Campus
+1/13/2015
+Prompting
+ */
+//alert("Testing to see if this works");
+
+// ask user for input - prompt()
+// save the users response - by creating a variable
+var userInput = prompt("Enter your year of birth");
+console.log(userInput);
+
+//Calculate the area of a rectangle
+// area = length * width
+
+//Create 2 variables and prompt the user for length and width
+var length = prompt("Let's calculate the area of a rectangle. \nPlease enter in length:");
+var width = prompt("Please enter in width:");
+
+//Calculate area
+var areaRec = length * width;
+console.log("The area of your rectangle with a length of "+length+" and a width of "+width+" is " +areaRec);
+
+
