706b11485a2634daf8971a9083b1e41cd324e8ce
Blake <bgeiger@fullsail.edu>
2015-01-13 23:05:53 -0500

    Equations added
    Input numbers to solve equations. Used arrays and assignment operators

--- Geiger_Blake_Expressions_Assignment/js/Geiger_Blake_Expressions_Assignment.js
+++ Geiger_Blake_Expressions_Assignment/js/Geiger_Blake_Expressions_Assignment.js
@@ -18,3 +18,18 @@ Expressions Assignment
  var width = prompt("What is the width?");
  console.log(width);
 
+ //Area equation from array
+ mathNeeded [0] = length * width;
+ console.log(mathNeeded[0]);
+
+ //Perimeter length from array
+ mathNeeded [1] = (length *= 2);
+ console.log(mathNeeded[1]);
+
+ //Perimeter width from array
+ mathNeeded [2] = (width *= 2);
+ console.log(mathNeeded[2]);
+
+ //Total perimeter from array
+ mathNeeded[3] = mathNeeded [1] += mathNeeded[2];
+ console.log(mathNeeded[3]);
