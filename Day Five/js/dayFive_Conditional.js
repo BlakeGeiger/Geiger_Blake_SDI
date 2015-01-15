9dcc9c0da6ccae0d3e6267a7d5c48837882094a9
Blake <bgeiger@fullsail.edu>
2015-01-15 18:55:38 -0500

    True boolean
    curly brackets not skipped

--- Day Five/js/dayFive_Conditional.js
+++ Day Five/js/dayFive_Conditional.js
@@ -5,4 +5,29 @@ SDI Campus
 1/15/2015
 Conditionals
  */
-alert("Testing to see if this works");
+//alert("Testing to see if this works");
+
+ // Basic conditional statement
+
+ // create a boolean variable to test
+
+var oldEnough = true;
+
+ //if the child is old enough then print to the console, you can ride!
+
+ /*
+ Basic structure of if statement
+
+ If(condition to test){
+ Code to run if the condition is true
+ }
+
+  */
+
+ if(oldEnough){
+//Code inside of curly brackets will run if the test is true
+  //this code will be skipped if the test is false
+  console.log("You can ride the coaster!");
+ }
+
+ console.log("What comes after the if statement");
