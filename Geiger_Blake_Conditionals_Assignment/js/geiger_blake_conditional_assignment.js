42e4e0c5c8cad80dc6308fd7bd9d2daf0db99c7b
Blake <bgeiger@fullsail.edu>
2015-01-20 16:53:27 -0500

    Idea and variables
    Idea for assignment and variables added

---
+++ Geiger_Blake_Conditionals_Assignment/js/geiger_blake_conditional_assignment.js
@@ -0,0 +1,18 @@
+
+ /*
+Blake Geiger
+SDI Campus
+1/18/2015
+Conditional Assignment
+ */
+//alert("Testing to see if this works");
+
+ //Go to a concert that you don't know how old you must be to get in
+ var userAge = prompt("In order to get into the show we must know your age, how old are you?");
+ var feeNot21 = 5;
+ var cost = 15;
+
+
+
+
+
