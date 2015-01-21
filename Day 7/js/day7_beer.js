9504001a1a29a1de9d633e90e16f7d5ffe0b0ada
Blake <bgeiger@fullsail.edu>
2015-01-20 19:20:46 -0500

    beer song
    complete beer song using loops

--- Day 7/js/day7_beer.js
+++ Day 7/js/day7_beer.js
@@ -4,4 +4,18 @@
  *Beer
  */
 
-alert("testing to make sure this works");
+//alert("testing to make sure this works");
+
+// 99 bottles of beer on the wall
+
+for(var i =99; i>0; i--){
+    
+    
+    //add a check for the last round of beers
+    if (i===1) {
+        console.log(i+ " bottle of beer on the wall, " +i+ " bottle of beer, you take one down, and pass it around, no more bottles of beer on the wall. \nThe End!");
+    } else{
+        console.log(i+ " bottles of beer on the wall, " +i+ " bottles of beer, you take one down, and pass it around, " +(i-1)+ " bottles of beer on the wall");
+    }
+}
+
