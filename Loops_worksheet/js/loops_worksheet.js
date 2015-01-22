31f5296011e2076e4dd3d499771688273eb698b3
Blake <bgeiger@fullsail.edu>
2015-01-22 11:25:40 -0500

    while loop
    need to fix validation

---
+++ Loops_worksheet/js/loops_worksheet.js
@@ -0,0 +1,29 @@
+/*
+ *Blake Geiger
+ *1/20/2015
+ *Loops Worksheet
+ */
+
+//alert("Testing to make sure this works");
+
+//while loop
+
+/*
+var i = 0
+while(condition){
+    //code that runs while condition is true
+    incremental change to the counting variable -
+    OR infinite loop
+}
+*/
+
+var states = prompt("How many states are in America?");
+while (states === 50){ 
+    //type in error message in case its wrong
+    states = prompt("That was incorrect, try again");
+}
+//make sure the user typed in a number
+while(isNaN(states)){
+    states = prompt("only use numbers please enter your answer:");
+    console.log(states);
+}
