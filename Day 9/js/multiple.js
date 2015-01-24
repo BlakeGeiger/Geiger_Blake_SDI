f7f2d6243197c1abaff56200fc64dd360bd47d77
Blake <bgeiger@fullsail.edu>
2015-01-24 18:33:53 -0500

    day 9 notes
    multiples

---
+++ Day 9/js/multiple.js
@@ -0,0 +1,7 @@
+/*
+ *Blake Geiger
+ *1/24/2015
+ *Mulitple
+ */
+
+alert("testing");
--- Day 9/js/procedure.js
+++ Day 9/js/procedure.js
@@ -4,4 +4,32 @@
  *Procedure 
  */
 
-alert("testing");
+//alert("testing");
+
+//this is a basic function
+function calcArea(width, height){
+    var area = (width*height);
+    return area;
+
+}
+
+//function call
+//create a avriable to catch the retun results 
+var results = calcArea(2,210);
+console.log(results);
+
+//this is a procedure
+//it does not return a value
+//list of steps to do
+function calcAreaP(width,height){
+    var area=width*height;
+    console.log(area);
+    //it will not have a return
+    
+}
+
+//function call for procedure
+calcAreaP(6,7);
+
+alert("test"); //procedure
+var userInput = prompt("aska a question");//prompting is function
