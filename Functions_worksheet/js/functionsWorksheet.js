5fe8c47481b2cc8bbda62ed0c1aeac02a7a1bb66
Blake <bgeiger@fullsail.edu>
2015-01-24 15:14:01 -0500

    functions worksheet
    function complete for circumference problem

---
+++ Functions_worksheet/js/functionsWorksheet.js
@@ -0,0 +1,18 @@
+/*
+ *Blake Geiger
+ *1/22/2015
+ *Functions Worksheet
+ */
+
+//alert("testing");
+
+//create a function to calc the circumference of a circle
+//circumference = 2*pi*r
+
+function circleCir(r){
+    var cir = Math.PI*2*r;
+    //return this circumference
+    return cir;
+    
+}
+
