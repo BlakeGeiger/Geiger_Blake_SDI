58223e569bc5dbf5b4b3df499f0fe215ffcfb6d1
Blake <bgeiger@fullsail.edu>
2015-01-27 16:54:32 -0500

    Fixed errors
    updated

--- Geiger_Blake_Functions_Assignment/js/functions_assignment.js
+++ Geiger_Blake_Functions_Assignment/js/functions_assignment.js
@@ -7,20 +7,20 @@
 //alert("testing");
 
 function area(w,h){
-    //area of a rectangle is width * height 
-    var area = w*h;
+    //area of a triangle is .5 * width * height 
+    var area = .5*w*h;
     return area;
 }
 
 //prompt for width and height
-var w = prompt("Please enter the width of you rectangle");
+var w = prompt("Please enter the width of you triangle");
 //validate nothing
 while(w===""){
     //reprompt
     w=prompt("Do not leave blank, enter the width as a number.");
 }
 //validate whether it's a number or not
-var h = prompt("Please enter the height of your rectangle");
+var h = prompt("Please enter the height of your tritangle");
 //validate nothing
 while(h===""){
     h=prompt("Do not leave blank, enter the height as a number.");
@@ -31,7 +31,7 @@ while(h===""){
 var totalArea = area(w,h);
 console.log(totalArea);
 
-function volume(l,w,h){
+var volumeAnon = function(l,w,h){
      //volume of a rectangle is length * width * height
     var volume = l*w*h;
     return volume;
@@ -60,5 +60,5 @@ while(h===""){
 //validate whether it's a number or not
 
 //call
-var totalVol = volume(l,w,h);
+var totalVol = volumeAnon(l,w,h);
 console.log(totalVol);
