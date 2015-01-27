8eb937f17c2412ea6e39d2f616e5db9b5ae406de
Blake <bgeiger@fullsail.edu>
2015-01-27 17:03:55 -0500

    code cleaned up
    errors cleaned, and validations updated

--- Geiger_Blake_Functions_Assignment/js/functions_assignment.js
+++ Geiger_Blake_Functions_Assignment/js/functions_assignment.js
@@ -15,21 +15,21 @@ function area(w,h){
 //prompt for width and height
 var w = prompt("Please enter the width of you triangle");
 //validate nothing
-while(w===""){
+while(w==="" || isNaN(w)){
     //reprompt
     w=prompt("Do not leave blank, enter the width as a number.");
 }
 //validate whether it's a number or not
 var h = prompt("Please enter the height of your tritangle");
 //validate nothing
-while(h===""){
+while(h==="" || isNaN(h)){
     h=prompt("Do not leave blank, enter the height as a number.");
 }
 //validate whether it's a number or not
 
 //call
 var totalArea = area(w,h);
-console.log(totalArea);
+console.log("The area of a triangle with a width of "+w+" and a height of "+h+" is " +totalArea+".");
 
 var volumeAnon = function(l,w,h){
      //volume of a rectangle is length * width * height
@@ -40,25 +40,25 @@ var volumeAnon = function(l,w,h){
 //prompt for length, width and height
 var l = prompt("Please enter the length of you rectangle");
 //validate nothing
-while(l===""){
+while(l==="" || isNaN(l)){
     //reprompt
     l=prompt("Do not leave blank, enter the length as a number.");
 }
 //validate whether it's a number or not
 var w = prompt("Please enter the width of you rectangle");
 //validate nothing
-while(w===""){
+while(w==="" || isNaN(w)){
     //reprompt
     w=prompt("Do not leave blank, enter the width as a number.");
 }
 //validate whether it's a number or not
 var h = prompt("Please enter the height of your rectangle");
 //validate nothing
-while(h===""){
+while(h==="" || isNaN(h)){
     h=prompt("Do not leave blank, enter the height as a number.");
 }
 //validate whether it's a number or not
 
 //call
 var totalVol = volumeAnon(l,w,h);
-console.log(totalVol);
+console.log("The volume of a rectangle witha length of "+l+", a width of "+w+", and a height of "+h+" is "+totalVol+".");
